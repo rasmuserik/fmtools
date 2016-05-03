7adc6b67256d9d706cab06c60f7ae51ddeaf5de8
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2016-05-03 18:04:06 +0200

    fm-tools better form

--- src/solsort/fmtools/main.cljs
+++ src/solsort/fmtools/main.cljs
@@ -28,7 +28,6 @@
 (register-handler
   :template
   (fn  [db  [_ id template]]
-    (log 'here)
     (dispatch [:sync-to-disk])
     (assoc-in db [:templates id] template)))
 
@@ -73,9 +72,10 @@
    2 :line
    3 :footer})
 (defonce line-types
-  {0  :simple-headline
-   1  :vertical-headline
-   2  :horizontal-headline
+  {0  :basic
+   1  :simple-headline
+   2  :vertical-headline
+   3  :horizontal-headline
    4  :multi-field-line
    5  :description-line
    10 :template-control})
@@ -83,51 +83,54 @@
 ;;
 ;; ## Synchronization with API
 ;;
-;; ## Styling
-;;
-(load-style!
-  {:.float-right
-   {:float :right}
-   :.right
-   {:text-align :right}
-   ".camera-input img"
-   {:height 44
-    :width 44
-    :padding 4
-    :border "2px solid black"
-    :border-radius 6
-    }
-   ".camera-input input"
-   {}
-   ".fmfield"
-   {;:display :inline-block
-    ;:margin "1em"
-    ;:padding "1em"
-    ; :border "1px solid black"
-
-    }
-   ".line"
-   {:margin "1em"
-    :padding "1em"
-    :border "1px solid black" }
-   ".checkbox"
-   {:display :inline-block
-    :border "2px solid black"
-    :border-radius 8
-    :font-size 32
-    :line-height 28
-    :margin 8
-    :width "32px"
-    :height "32px"
-    }
-   }
-  "basic-style")
-
-
 ;; ## Components
 ;;
-;; ### Camera button
+;; ### Main App entry point
 ;;
+
+(declare app)
+(defonce unit (atom 10))
+(defn style []
+  (reset! unit (js/Math.floor (* 0.90 (/ 1 12) js/window.innerWidth)))
+  (log 'style @unit)
+  (let [unit @unit]
+    (load-style!
+      {:.camera-input
+       {:display :inline-block
+        :position :absolute
+        :right 0
+        }
+       :.fmfield 
+       {:clear :right }
+       :.checkbox
+       { :display :inline-block
+        :border "2px solid black"
+        :border-radius (* unit .25)
+        :font-size unit
+        :line-height (* unit .8)
+        :margin (* unit .25)
+        :width unit
+        :height unit}
+       :.multifield
+       {:border-bottom "0.5px solid #ccc"}
+      ".camera-input img"
+      {:height unit
+       :width unit
+       :padding 4
+       :border "2px solid black"
+       :border-radius 6
+       :opacity "0.5"
+       }
+      :.fields
+      {:text-align :center }
+       }
+      "check-style"
+      ))
+  (render [app])
+  )
+(aset js/window "onresize" style)
+(js/setTimeout style 0)
+
 (defn camera-button []
   (let [id (str "camera" (js/Math.random))]
     (fn []
@@ -135,21 +138,24 @@
        [:label {:for id}
         [:img.camera-button {:src "assets/camera.png"}]]
        ; TODO apparently :camera might not be a supported property in react
-       [:input {:type "file" :capture "camera" :accept "image/*" :id id}]
+       [:input {:type "file" :capture "camera" :accept "image/*" :id id :style {:display :none}}]
        ])))
-;;
-;; ### Main App entry point
-;;
+
 (defn checkbox [id]
   [:span.checkbox
-   ;"✓"
-   (if (< 0.9 (js/Math.random)) "\u00a0" "✔")
-   ]
-  )
-(defn field [field]
-  (let [id (:FieldGuid field)]
+   (if (< 0.7 (js/Math.random)) "\u00a0" "✔")])
+
+(defn field [field cols]
+  (let [id (:FieldGuid field) ]
     [:span.fmfield {:key id
-                    :on-click (fn [] (js/alert (str field)) false)}
+                    :style 
+                    {:width (* 12 @unit (/ (:Columns field) cols))
+                     :vertical-align :top
+                     :display :inline-block
+                     ;:border-left "1px solid black"
+                     ;:border-right "1px solid black"
+                     :text-align :center}
+                    :on-click (fn [] (log field) false)}
      (case (:FieldType field)
        :text-fixed [:span.text-fixed-frame (:FieldValue field)]
        :text-input [:input {:type :text :name (:FieldGuid field)}]
@@ -160,7 +166,7 @@
        (if (:DoubleField field)
          [:span
           [checkbox (:FieldGuid field)] " / "
-          [checkbox (:FieldGuid field)] ]
+          [checkbox (:FieldGuid field)] " \u00a0 "]
          [checkbox (:FieldGuid field)])
        :text-fixed-noframe [:span.text-fixed-noframe (:FieldValue field)]
        [:strong "unhandled field:"
@@ -170,23 +176,29 @@
      ]))
 
 (defn line [line]
-  (let [id (:PartGuid line)]
+  (let [id (:PartGuid line)
+        cols (apply + (map :Columns (:fields line)))
+        desc (:TaskDescription line)
+        fields (into
+                 [:div.fields]
+                 (map #(field % cols)  (:fields line)))]
     [:div.line
-     {:key id
-      :on-click #(js/alert (str (dissoc line :fields)))}
+     {:style
+      {:padding-top 10
+       }
+      :key id
+      :on-click #(log (dissoc line :fields))}
      (case (:LineType line)
-       :simple-headline [:h3 "_ " (:TaskDescription line)]
+       :basic [:h3 "." (:TaskDescription line)]
+       :simple-headline [:h3 (:TaskDescription line)]
        ;:vertical-headline [:h3.vertical (:TaskDescription line)]
-       :vertical-headline (into [:div [:h3.vertical ". "
-                                       (:TaskDescription line)]]
-                                (map field (:fields line)))
-       :horizontal-headline (into [:div [:h3.vertical ", "
-                                         (:TaskDescription line)]]
-                                  (map field (:fields line)))
-       :multi-field-line (into [:div "* " (:TaskDescription line) [:br]]
-                               (map field (:fields line)))
-       [:strong {:key id} "unhandled line " (str (:LineType line)) " "
-        (:FieldValue field)])
+       :vertical-headline [:div [:h3 desc] fields]
+       :horizontal-headline [:div [:h3 desc ] fields]
+       :multi-field-line [:div.multifield desc [camera-button id ] 
+                          fields ]
+       :description-line [:div desc [:input {:type :text}]]
+       [:span {:key id} "unhandled line " (str (:LineType line)) " "
+        (str (dissoc line :fields))])
      ]))
 
 (defn render-template [id]
@@ -196,10 +208,7 @@
       [:div.ui.form
        [:h1 (:Description template)]]
       (map line (:rows template))
-      [:pre
-       (js/JSON.stringify (clj->js template) nil 2)]
-      ;[:pre (str (cljs.pprint/pprint template))]
-
+      ;[:pre (js/JSON.stringify (clj->js template) nil 2)]
       )))
 
 (defn form []
@@ -212,16 +221,13 @@
     ))
 
 (defn app []
-  [:div.ui.container
+  [:div
    [:h1 "FM-Tools"]
    [:hr]
    [form]
-   ;[:div.right [camera-button]]
-   [:hr]])
+   ])
 
 ;; ## Execute and events
-
-(render [app])
 
 (defn <api [endpoint]
   (<ajax (str "https://"
@@ -251,7 +257,7 @@
                            (sort-by :DisplayOrder
                                     (get fields (:PartGuid part)))))
                   (sort-by :DisplayOrder parts))
-          parts (map #(assoc % :LineType (line-types (:LineType %))) parts)
+          parts (map #(assoc % :LineType (or (line-types (:LineType %)) (log "invalid-LintType" %))) parts)
           parts (map #(assoc % :PartType (part-types (:PartType %))) parts)
           ]
       (dispatch [:template template-id (assoc template :rows parts)]))))
