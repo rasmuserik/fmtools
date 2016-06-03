356ff29f11dd280c3d3a37680f51426c8e8f63af
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2016-06-03 16:02:20 +0200

    remove manifest-test

--- manifest-test/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -vx
-
-# Manifest file
-#
-echo "CACHE MANIFEST" > index.appcache
-echo "# `date`" >> index.appcache
-find assets -type f >> index.appcache
-echo "index.js" >> index.appcache
--- manifest-test/index.js
+++
@@ -1,6 +0,0 @@
-document.write('hello 4');
-if(window.applicationCache) {
-  window.applicationCache.onupdateready = function() { 
-    location.reload();
-  };
-}
