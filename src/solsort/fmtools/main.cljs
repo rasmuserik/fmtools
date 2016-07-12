6ad64756a746586a9f44d6c024ba50308037244e
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2016-07-12 15:24:07 +0200

    ability to clear db-content via hash-parameter

--- src/solsort/fmtools/main.cljs
+++ src/solsort/fmtools/main.cljs
@@ -4,6 +4,7 @@
    [cljs.core.async.macros :refer [go]])
   (:require
    [cljs.core.async :as async :refer [<!]]
+   [solsort.util :refer [<p]]
    [solsort.fmtools.util]
    [solsort.fmtools.db :as db]
    [solsort.fmtools.ui]
@@ -12,9 +13,9 @@
 
 (defonce load-data
   (go
+    (when (not= -1 (.indexOf js/location.hash "reset"))
+      (<! (<p (js/localforage.clear))))
     (<! (disk/<restore-form))
     (<! (api/<fetch))
     (<! (disk/<save-form))
-    (disk/watch-changes! :data) 
-    ))
-
+    (disk/watch-changes! :data)))
